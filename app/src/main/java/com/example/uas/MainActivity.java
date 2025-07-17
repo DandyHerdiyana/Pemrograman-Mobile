1288d3d965bd209b3424ed8598edb0f623a6577b
Potato <dandyherdiyana@gmail.com>
2025-07-18 02:46:34 +0700

    //build fix! app can run!

--- app/src/main/java/com/example/uas/MainActivity.java
+++ app/src/main/java/com/example/uas/MainActivity.java
@@ -1,37 +1,36 @@
 package com.example.uas;
 
 import android.os.Bundle;
-
 import android.view.View;
-import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.uas.FirstFragment;
-import com.example.uas.SecondFragment;
+import com.google.android.material.card.MaterialCardView;
 
 public class MainActivity extends AppCompatActivity {
 
-	Button firstFragment, secondFragment;
+	MaterialCardView cardFirst, cardSecond;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		firstFragment = (Button) findViewById(R.id.firstFragment);
-		secondFragment = (Button) findViewById(R.id.secondFragment);
+		// Link the cards from layout
+		cardFirst = findViewById(R.id.cardFirst);
+		cardSecond = findViewById(R.id.cardSecond);
 
-		firstFragment.setOnClickListener(new View.OnClickListener() {
+		// Set listener for the first fragment
+		cardFirst.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				loadFragment(new FirstFragment());
 			}
 		});
 
-		secondFragment.setOnClickListener(new View.OnClickListener() {
+		// Set listener for the second fragment
+		cardSecond.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View view) {
 				loadFragment(new SecondFragment());
@@ -39,10 +38,11 @@ public class MainActivity extends AppCompatActivity {
 		});
 	}
 
+	// Function to load fragments into the frame layout
 	private void loadFragment(Fragment fragment) {
 		FragmentManager fm = getSupportFragmentManager();
-		FragmentTransaction fragmentTransaction = fm.beginTransaction();
-		fragmentTransaction.replace(R.id.frameLayout, fragment);
-		fragmentTransaction.commit();
+		FragmentTransaction transaction = fm.beginTransaction();
+		transaction.replace(R.id.frameLayout, fragment);
+		transaction.commit();
 	}
 }
