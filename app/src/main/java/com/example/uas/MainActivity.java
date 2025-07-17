d54db7a5b9c46301b7934250e18f22b56fab4482
Potato <dandyherdiyana@gmail.com>
2025-07-18 03:11:24 +0700

    //functional! final?

--- app/src/main/java/com/example/uas/MainActivity.java
+++ app/src/main/java/com/example/uas/MainActivity.java
@@ -2,44 +2,44 @@ package com.example.uas;
 
 import android.os.Bundle;
 import android.view.View;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import com.google.android.material.card.MaterialCardView;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-	MaterialCardView cardFirst, cardSecond;
+	Button firstFragment, secondFragment;
+	LinearLayout mainContent;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// Link the cards from layout
-		cardFirst = findViewById(R.id.cardFirst);
-		cardSecond = findViewById(R.id.cardSecond);
+		firstFragment = findViewById(R.id.firstFragment);
+		secondFragment = findViewById(R.id.secondFragment);
+		mainContent = findViewById(R.id.mainContent); // this is the wrapper
 
-		// Set listener for the first fragment
-		cardFirst.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				loadFragment(new FirstFragment());
-			}
+		firstFragment.setOnClickListener(view -> {
+			loadFragment(new FirstFragment());
 		});
 
-		// Set listener for the second fragment
-		cardSecond.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				loadFragment(new SecondFragment());
-			}
+		secondFragment.setOnClickListener(view -> {
+			loadFragment(new SecondFragment());
 		});
 	}
 
-	// Function to load fragments into the frame layout
 	private void loadFragment(Fragment fragment) {
+		// Hide main layout
+		if (mainContent != null) {
+			mainContent.setVisibility(View.GONE);
+		}
+
+		// Show fragment
 		FragmentManager fm = getSupportFragmentManager();
 		FragmentTransaction transaction = fm.beginTransaction();
 		transaction.replace(R.id.frameLayout, fragment);
